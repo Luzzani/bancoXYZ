34599040d6da77dca48fe25dcb56e8d277b45d5b
Lucas Martin Luzzani <lucasluzzani@gmail.com>
2026-03-13 19:52:32 -0300

    chore: initial redux setup with provider and store index

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { RootNavigator } from './src/navigation/RootNavigator';
+import { Provider } from 'react-redux';
+import { store } from './src/store';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <RootNavigator />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </Provider>
   );
 }
