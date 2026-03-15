d3538aecf3ef2353e8ffab4eff3ab38bc28833af
Lucas Martin Luzzani <lucasluzzani@gmail.com>
2026-03-15 12:33:16 -0300

    fix: correct folder  naming in trasnfer feature

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,12 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/auth';
 import balanceReducer from './slices/balance';
 import { injectStore } from '../api/apiClient';
-import trasnferReducer from './slices/trasnfer';
+import transferReducer from './slices/transfer';
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     balance: balanceReducer,
-    trasnfer: trasnferReducer,
+    transfer: transferReducer,
   },
 });
 
